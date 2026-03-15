3802075a66001f51c1ae273122ce814e74453a93
RapliVX <raffikun1203@gmail.com>
2026-03-15 23:46:40 +0700

    Revert "ida: Add new API"
    This reverts commit c9c5865264ae7c2414d94050410d0c80d3bcdbd2.

--- lib/idr.c
+++ lib/idr.c
@@ -199,8 +199,7 @@ EXPORT_SYMBOL(idr_replace_ext);
  * bit per ID, and so is more space efficient than an IDR.  To use an IDA,
  * define it using DEFINE_IDA() (or embed a &struct ida in a data structure,
  * then initialise it using ida_init()).  To allocate a new ID, call
- * ida_alloc(), ida_alloc_min(), ida_alloc_max() or ida_alloc_range().
- * To free an ID, call ida_free().
+ * ida_simple_get().  To free an ID, call ida_simple_remove().
  *
  * If you have more complex locking requirements, use a loop around
  * ida_pre_get() and ida_get_new() to allocate a new ID.  Then use
@@ -261,7 +260,7 @@ EXPORT_SYMBOL(idr_replace_ext);
  * Allocate new ID above or equal to @start.  It should be called
  * with any required locks to ensure that concurrent calls to
  * ida_get_new_above() / ida_get_new() / ida_remove() are not allowed.
- * Consider using ida_alloc_range() if you do not have complex locking
+ * Consider using ida_simple_get() if you do not have complex locking
  * requirements.
  *
  * If memory is required, it will return %-EAGAIN, you should unlock
@@ -431,34 +430,43 @@ void ida_destroy(struct ida *ida)
 EXPORT_SYMBOL(ida_destroy);
 
 /**
- * ida_alloc_range() - Allocate an unused ID.
- * @ida: IDA handle.
- * @min: Lowest ID to allocate.
- * @max: Highest ID to allocate.
- * @gfp: Memory allocation flags.
+ * ida_simple_get - get a new id.
+ * @ida: the (initialized) ida.
+ * @start: the minimum id (inclusive, < 0x8000000)
+ * @end: the maximum id (exclusive, < 0x8000000 or 0)
+ * @gfp_mask: memory allocation flags
  *
- * Allocate an ID between @min and @max, inclusive.  The allocated ID will
- * not exceed %INT_MAX, even if @max is larger.
+ * Allocates an id in the range start <= id < end, or returns -ENOSPC.
+ * On memory allocation failure, returns -ENOMEM.
  *
- * Context: Any context.
- * Return: The allocated ID, or %-ENOMEM if memory could not be allocated,
- * or %-ENOSPC if there are no free IDs.
+ * Compared to ida_get_new_above() this function does its own locking, and
+ * should be used unless there are special requirements.
+ *
+ * Use ida_simple_remove() to get rid of an id.
  */
-int ida_alloc_range(struct ida *ida, unsigned int min, unsigned int max,
-			gfp_t gfp)
+int ida_simple_get(struct ida *ida, unsigned int start, unsigned int end,
+		   gfp_t gfp_mask)
 {
 	int ret, id;
+	unsigned int max;
 	unsigned long flags;
 
-	if ((int)min < 0)
-		return -ENOSPC;
+	BUG_ON((int)start < 0);
+	BUG_ON((int)end < 0);
 
-	if ((int)max < 0)
-		max = INT_MAX;
+	if (end == 0)
+		max = 0x80000000;
+	else {
+		BUG_ON(end < start);
+		max = end - 1;
+	}
 
 again:
+	if (!ida_pre_get(ida, gfp_mask))
+		return -ENOMEM;
+
 	xa_lock_irqsave(&ida->ida_rt, flags);
-	ret = ida_get_new_above(ida, min, &id);
+	ret = ida_get_new_above(ida, start, &id);
 	if (!ret) {
 		if (id > max) {
 			ida_remove(ida, id);
@@ -469,24 +477,24 @@ again:
 	}
 	xa_unlock_irqrestore(&ida->ida_rt, flags);
 
-	if (unlikely(ret == -EAGAIN)) {
-		if (!ida_pre_get(ida, gfp))
-			return -ENOMEM;
+	if (unlikely(ret == -EAGAIN))
 		goto again;
-	}
 
 	return ret;
 }
-EXPORT_SYMBOL(ida_alloc_range);
+EXPORT_SYMBOL(ida_simple_get);
 
 /**
- * ida_free() - Release an allocated ID.
- * @ida: IDA handle.
- * @id: Previously allocated ID.
+ * ida_simple_remove - remove an allocated id.
+ * @ida: the (initialized) ida.
+ * @id: the id returned by ida_simple_get.
  *
- * Context: Any context.
+ * Use to release an id allocated with ida_simple_get().
+ *
+ * Compared to ida_remove() this function does its own locking, and should be
+ * used unless there are special requirements.
  */
-void ida_free(struct ida *ida, unsigned int id)
+void ida_simple_remove(struct ida *ida, unsigned int id)
 {
 	unsigned long flags;
 
@@ -497,4 +505,4 @@ void ida_free(struct ida *ida, unsigned int id)
 	ida_remove(ida, id);
 	xa_unlock_irqrestore(&ida->ida_rt, flags);
 }
-EXPORT_SYMBOL(ida_free);
+EXPORT_SYMBOL(ida_simple_remove);
